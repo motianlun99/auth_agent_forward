4d5b2dc4f065aed5144767b8b9bb8d09e8fdc16b
motianlun99 <464716448@qq.com>
2020-02-04 17:35:14 +0800

    Add files via upload
    first commit

---
+++ auth_agent_forward/src/test/java/com/duowan/auth_agent_forward/AuthAgentForwardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duowan.auth_agent_forward;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthAgentForwardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
