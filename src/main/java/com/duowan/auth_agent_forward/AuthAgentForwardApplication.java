41bead9966a19ba0a8fdd9a579aa0e9ce9a4e351
songjian1 <songjian1@yy.com>
2020-02-04 17:39:25 +0800

    first init

---
+++ src/main/java/com/duowan/auth_agent_forward/AuthAgentForwardApplication.java
@@ -0,0 +1,13 @@
+package com.duowan.auth_agent_forward;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthAgentForwardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthAgentForwardApplication.class, args);
+	}
+
+}
